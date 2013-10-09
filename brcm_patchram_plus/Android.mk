be58af7ab79645363f0ca2055e19cf1527c3a51d
CodeKill13 <codekill13@gmail.com>
2013-10-09 13:26:58 -0400

    Bullshizz

--- brcm_patchram_plus/Android.mk
+++ brcm_patchram_plus/Android.mk
@@ -8,6 +8,14 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifeq ($(TARGET_NEEDS_BLUETOOTH_INIT_DELAY),true)
+LOCAL_CFLAGS += -DBCM_INIT_DELAY
+endif
+
+ifeq ($(BOARD_HAVE_SAMSUNG_BLUETOOTH),true)
+    LOCAL_CFLAGS += -DSAMSUNG_BLUETOOTH
+endif
+
 LOCAL_SRC_FILES := brcm_patchram_plus.c
 
 LOCAL_MODULE := brcm_patchram_plus
